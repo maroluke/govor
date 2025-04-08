34bf32d25860a9cf0d0792abfc90f0f07e972619
Marko Lukac <marko.lukac@clustertec.com>
2025-04-08 10:02:16 +0200

    add footer

--- app.vue
+++ app.vue
@@ -3,9 +3,14 @@
     <main>
       <ButtonGrid />
     </main>
+    <div class="flex gap-2 justify-center items-center pt-5 pb-16">
+      <Heart class="fill-red-500 stroke-red-500" /> Najdražoj ujni, za brz
+      oporavak!
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import ButtonGrid from "~/components/ButtonGrid.vue";
+import { Heart } from "lucide-vue-next";
 </script>
